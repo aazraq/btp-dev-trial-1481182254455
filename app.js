0e74bc310c8331e4855469759607560f0fcae287
Ahmed Azraq <aazraq@eg.ibm.com>
2016-12-08 07:50:17 -0600

    Trying environment variables.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*eslint-env node*/
+/*eslint-env node, express*/
 
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
@@ -12,17 +12,51 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
 // create a new express server
 var app = express();
+
+var httpRequest = require('request');
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+
+app.get('/getAuthor', function(req, res) {
+	console.log("Hello Author");
+
+	console.log("VCAP");
+	console.log(process.env);
+	var username = 'wim@antwerpportauthority.be';
+	var password = 'wbid01bm';
+	var authData =  "Basic " + new Buffer(username + ":" + password).toString("base64");
+	var options = {
+		url: 'https://api.us.apiconnect.ibmcloud.com/aazraqegibmcom-svp-dev/chain2-catalog/SVPService/locations',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/xml',
+			'Authorization':authData,
+			'x-ibm-client-id':'078bd6b7-dfcc-4c58-8b6c-061b731e9129'
+		},
+		form: '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:wsdl="http://svp.poc.com/wsdl"><soapenv:Header/><soapenv:Body><wsdl:locations/></soapenv:Body></soapenv:Envelope>'
+	};
+	httpRequest(
+		options,
+		function(error, response, body) {
+				console.log("body");
+				console.log(body);
+				console.log("error");
+				console.log(error);
+		}
+	);
+});
+
+
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+	// print a message when the server starts listening
+	console.log("server starting on " + appEnv.url);
 });
