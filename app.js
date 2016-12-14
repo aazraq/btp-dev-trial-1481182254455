40417601631e6ca15f15a1a6a2729e8ac86b1873
Mohamed Khosrhid <mohamed.wagdy@eg.ibm.com>
2016-12-14 06:45:26 -0600

    Fixing merge issues

--- app.js
+++ app.js
@@ -29,19 +29,6 @@ var etbCalculator = require('./etbCalculator');
 app.use(express.static(__dirname + '/public'));
 
 
-<<<<<<< Upstream, based on b7628374f8997adf183bff5878af17580e8e6f1f
-app.post('/calculateETB', function(req, res) {
-	var objectId = req.body.objectId;
-	console.log(objectId);
-	var isWeather = req.body.isWeather;
-	console.log(isWeather);
-	if (isWeather !== null && isWeather === 'on') {
-		res.send('Weather');
-	} else {
-		etbCalculator.queryEvent(objectId, function(error, event) {
-			if (error) {
-				res.send('Please try again');
-=======
 app.get('/calculateETB', function(req, res) {
 	var objectId = req.query.objectId;
 	console.log(objectId);
@@ -57,7 +44,6 @@ app.get('/calculateETB', function(req, res) {
 			if (error) {
 				console.log(error);
 				res.status(500).send();
->>>>>>> 9ef8c35 Integrated front-end with node back-end + adjusted error handling from node.js
 			} else {
 				console.log("I am back");
 				console.log(event);
